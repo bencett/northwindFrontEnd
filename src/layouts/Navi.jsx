600e96f63b635d35395691d7bfa9527d0cd3eb9c
Çetin Ürün <cetinurun.12@outlook.com>
2021-06-10 00:08:58 +0300

    Product details and login/logout actions added.

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import CartSummary from './CartSummary';
-import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import { Container, Menu } from 'semantic-ui-react'
+import SignetOut from "./SignetOut";
+import SignedIn from "./SignedIn";
+import { useHistory } from "react-router";
 
 export default function Navi() {
+  const [isAuthanticated, setiIsAuthanticated] = useState(true)
+  const history = useHistory()
+  
+  function handleSignOut(params) {
+    setiIsAuthanticated(false)
+    history.push("/")
+  }
+  
+  function handleSignIn(params) {
+    setiIsAuthanticated(true)
+  }
   return (
     <div>
       <Menu inverted fixed="top">
+        <Container>
         <Menu.Item name="home"/>
         <Menu.Item name="messages"/>
 
         <Menu.Menu position="right">
          <CartSummary/>
-
-          <Menu.Item>
-            <Button primary>Sign Up</Button>
-          </Menu.Item>
+         {isAuthanticated?<SignedIn signOut={handleSignOut}/>
+         :<SignetOut signIn={handleSignIn}/>}  
         </Menu.Menu>
+        </Container> 
       </Menu>
     </div>
   );
---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Dropdown, Menu, Image } from "semantic-ui-react";
+
+export default function SignedIn({signOut}) {
+  return (
+    <div>
+      <Menu.Item>
+        <Image
+          avatar
+          spaced="right"
+          src="https://avatars.githubusercontent.com/u/48283331?v=4"
+        />
+        <Dropdown pointing="top left" text="Çetin">
+          <Dropdown.Menu>
+            <Dropdown.Item text="Bilgilerim" icon="info" />
+            <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out" />
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+    </div>
+  );
+}
